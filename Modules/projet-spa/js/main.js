26718d6df8c94bc043f60947f5c35bf697e8ec24
Romain Bohdanowicz <romain.bohdanowicz@formation.tech>
2022-11-14 14:23:50 +0100

    Modules

---
+++ Modules/projet-spa/js/main.js
@@ -0,0 +1,54 @@
+import { about } from './about.js';
+import { home } from './home.js';
+import { notFound } from './not-found.js';
+import { productDetails } from './product-details.js';
+import { products } from './products.js';
+import { search } from './search.js';
+
+const routes = [
+  {
+    hash: '#/',
+    render: home,
+  },
+  {
+    hash: '#/about',
+    render: about,
+  },
+  {
+    hash: '#/search',
+    render: search,
+  },
+  {
+    hash: '#/products',
+    render: products,
+  },
+  {
+    hash: '#/product-details',
+    render: productDetails,
+  },
+];
+
+function matchRoute() {
+  const mainEl = document.querySelector('body > main');
+
+  const match = routes.find((route) => route.hash === location.hash);
+
+  if (!match) {
+    return notFound(mainEl);
+  }
+
+  match.render(mainEl);
+
+  const links = document.querySelectorAll('header a');
+
+  for (const link of links) {
+    link.classList.remove('active');
+
+    if (link.href.endsWith(location.hash)) {
+      link.classList.add('active');
+    }
+  }
+}
+
+matchRoute();
+window.addEventListener('hashchange', matchRoute);
