6e559e174a624540c864eedbdcf8a97cc71872e7
Romain Bohdanowicz <romain.bohdanowicz@formation.tech>
2022-11-15 15:10:26 +0100

    Correction

--- Exercice-Build/build.js
+++ Exercice-Build/build.js
@@ -1,15 +1,15 @@
-const fs = require('fs/promises');
-const path = require('path');
-const md5 = require('md5');
-const { minify } = require('terser');
+const fs = require("fs/promises");
+const path = require("path");
+const md5 = require("md5");
+const { minify } = require("terser");
 
-const distPath = path.resolve(__dirname, 'dist');
-const srcPath = path.resolve(__dirname, 'src');
-const horlogeJsPath = path.resolve(srcPath, 'js', 'horloge.js');
-const indexJsPath = path.resolve(srcPath, 'js', 'index.js');
-const indexHtmlPath = path.resolve(srcPath, 'index.html');
-const indexHtmlDistPath = path.resolve(distPath, 'index.html');
-const appJsDistPath = path.resolve(distPath, 'app.js');
+const distPath = path.resolve(__dirname, "dist");
+const srcPath = path.resolve(__dirname, "src");
+const horlogeJsPath = path.resolve(srcPath, "js", "horloge.js");
+const indexJsPath = path.resolve(srcPath, "js", "index.js");
+const indexHtmlPath = path.resolve(srcPath, "index.html");
+const indexHtmlDistPath = path.resolve(distPath, "index.html");
+const appJsDistPath = path.resolve(distPath, "app.js");
 
 async function build() {
   await fs.rm(distPath, { force: true, recursive: true });
@@ -19,11 +19,28 @@ async function build() {
   // utiliser fs.readFile pour lire les fichiers
   // horlogeJsPath et indexJsPath
   // et fs.appendFile pour écrire dans appJsDistPath
+  const bufferHorloge = await fs.readFile(horlogeJsPath);
+  const bufferIndex = await fs.readFile(indexJsPath);
+  await fs.appendFile(appJsDistPath, bufferHorloge);
+  await fs.appendFile(appJsDistPath, bufferIndex);
+  // await fs.writeFile(appJsDistPath, Buffer.concat([bufferHorloge, bufferIndex]));
 
   // Exercice 4
   // fs.readFile et fs.writeFile pour écrire
   // buffer.toString('utf-8') pour convertir un buffer en string
   // str = str.replace('oldvalue', 'newvalue');
+  let contentIndexHtml = await fs.readFile(indexHtmlPath, {
+    encoding: "utf-8",
+  });
+
+  contentIndexHtml = contentIndexHtml
+    .replace(
+      '<script src="./js/horloge.js"></script>',
+      '<script src="./app.js"></script>'
+    )
+    .replace('<script src="./js/index.js"></script>', "");
+
+  await fs.writeFile(indexHtmlDistPath, contentIndexHtml);
 }
 
 build();
