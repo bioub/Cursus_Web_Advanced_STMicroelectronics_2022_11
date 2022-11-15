d050cc8c772b99a3c565c5baa67e78fc8023f65d
Romain Bohdanowicz <romain.bohdanowicz@formation.tech>
2022-11-15 14:23:22 +0100

    Suite Exercice

--- Exercice-Build/build.js
+++ Exercice-Build/build.js
@@ -11,3 +11,19 @@ const indexHtmlPath = path.resolve(srcPath, 'index.html');
 const indexHtmlDistPath = path.resolve(distPath, 'index.html');
 const appJsDistPath = path.resolve(distPath, 'app.js');
 
+async function build() {
+  await fs.rm(distPath, { force: true, recursive: true });
+  await fs.mkdir(distPath);
+
+  // Exercice 3
+  // utiliser fs.readFile pour lire les fichiers
+  // horlogeJsPath et indexJsPath
+  // et fs.appendFile pour écrire dans appJsDistPath
+
+  // Exercice 4
+  // fs.readFile et fs.writeFile pour écrire
+  // buffer.toString('utf-8') pour convertir un buffer en string
+  // str = str.replace('oldvalue', 'newvalue');
+}
+
+build();
