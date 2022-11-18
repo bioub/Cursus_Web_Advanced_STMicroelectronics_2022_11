7debf60022484c676bf3107e15c91746f02813d5
Romain Bohdanowicz <romain.bohdanowicz@formation.tech>
2022-11-18 16:28:02 +0100

    Exemple Express

--- Node.js/src/08-express.ts
+++ Node.js/src/08-express.ts
@@ -2,13 +2,28 @@ import express from 'express';
 
 const app = express();
 
+// middleware (plugin)
+// Body-Parser (parse le body en JSON)
+app.use(express.json());
+
 app.get('/', (req, res) => {
   // req et res héritent de IncomingMessage et ServerResponse
   res.send('<h2>Home page</h2>'); // send répond en HTML (et appelle end)
 });
 
 app.get('/api/json', (req, res) => {
-  res.json({ msg: 'Hello' });
+  res.json({ msg: 'Hello' }); // json répond en JSON (et appelle end)
+});
+
+// Tester avec GET /api/hello/Romain par exemple
+app.get('/api/hello/:name', (req, res) => {
+  const name = req.params.name;
+  res.json({ msg: 'Hello ' + name });
+});
+
+app.post('/api/register', (req, res) => {
+  console.log('POST');
+  console.log(req.body);
 });
 
 app.listen(3000, () => {
