85545539dfcf6887c5d192715e73069fc5c00ee4
Romain Bohdanowicz <romain.bohdanowicz@formation.tech>
2022-11-29 12:15:54 +0100

    Demarrage mocha

---
+++ Tests-Mocha/src/maths.spec.ts
@@ -0,0 +1,23 @@
+describe('maths.ts', () => {
+
+  describe('sum function', () => {
+
+    it('should add positive numbers', () => {
+  
+    });
+
+    it('should add negative numbers', () => {
+  
+    });
+    
+  });
+
+  describe('square function', () => {
+
+    it('should returns square value', () => {
+  
+    });
+    
+  });
+
+});
